0e9a94185801cbed81eeca48caa088238a84d216
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-11-03 14:59:38 +0900

    ok

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -16,7 +16,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <RouterProvider navigate={router.push}>
-      <ThemeProvider storageKey="theme-ui" attribute="class" defaultTheme="system">
+      <ThemeProvider storageKey="theme-ui" attribute="class" defaultTheme="light">
         {children}
       </ThemeProvider>
     </RouterProvider>
